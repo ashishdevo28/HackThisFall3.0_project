a7302cbfb5aa959a0d87edc20b2367a12faae58c
Blood Rogue <adnaptimsib333@gmail.com>
2023-02-04 13:19:59 +0530

    edit

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+from time import sleep
 
 news_items = []
 
@@ -11,4 +12,8 @@ for i in range(1, 11):
 print("\n".join(news_items))
 
 while True:
-    pass
+    data = requests.get(f"https://cryptonews.net/?page={i}").text
+    soup = BeautifulSoup(data, "html.parser")
+    recent_news_items = list(set(map(lambda a: a[1], filter(lambda classes: "news-item" in classes[0], map(lambda tag: [tag.get('class', ""), tag.get('data-title')], soup.body.main.find_all("div"))))).difference(news_items[:20]))
+    recent_news_items.extend(news_items)
+    sleep(60)
